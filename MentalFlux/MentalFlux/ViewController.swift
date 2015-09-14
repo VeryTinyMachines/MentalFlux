974ddfe426e71aa83f4c488016ccbe3337dfbcc8
Andrew J Clark <andrew.j.clark@me.com>
2015-09-14 10:24:39 +0930

    Initial xcode project

---
+++ MentalFlux/MentalFlux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MentalFlux
+//
+//  Created by Andrew J Clark on 14/09/2015.
+//  Copyright © 2015 Very Tiny Machines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
